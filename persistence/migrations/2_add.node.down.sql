f889034f0e540ad0de59450c00ea1be43ddb35bf
Pierre Beugnet <pierre.beugnet@nydig.com>
2022-11-09 17:03:39 +0100

    fix typo in sql down script

--- persistence/migrations/2_add.node.down.sql
+++ persistence/migrations/2_add.node.down.sql
@@ -1,9 +1,9 @@
 SET search_path TO lnmux;
 
-ALTER TABLE lnmux.invoices DROP COLUMN "node";
+ALTER TABLE lnmux.htlcs DROP COLUMN "node";
 
 -- Remove PRIMARY KEY
 ALTER TABLE lnmux.htlcs DROP CONSTRAINT htlcs_pkey;
 
 -- Set the new PRIMARY KEY
-ALTER TABLE lnmux.htlcs PRIMARY KEY (chan_id, htlc_id);
+ALTER TABLE lnmux.htlcs ADD PRIMARY KEY (chan_id, htlc_id);
